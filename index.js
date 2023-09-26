14df0d0d1bf1458cf2734407379866803b004713
ManneG <74592735+ManneG@users.noreply.github.com>
2023-09-26 16:57:33 +0200

    Put homepage in public folder served with static

--- index.js
+++ index.js
@@ -7,18 +7,20 @@ const router = require('./router.js');
 const http = require("http");
 const https = require("https");
 const fs = require("fs");
+const path = require('path')
 
 if(production) app.use((req, res, next) => {
     if(req.secure || req.query?.http != undefined) return next();
     res.redirect("https://manne.dev" + req.originalUrl);
 });
 
+app.use(express.static(path.join(__dirname, "public")))
 app.use(router);
 
 if(production){
     const httpsOptions = {
-        key: fs.readFileSync('/etc/letsencrypt/live/manne.dev/privkey.pem'),
-        cert: fs.readFileSync('/etc/letsencrypt/live/manne.dev/fullchain.pem'),
+        key: fs.readFileSync("/etc/letsencrypt/live/manne.dev/privkey.pem"),
+        cert: fs.readFileSync("/etc/letsencrypt/live/manne.dev/fullchain.pem"),
     };
 
     https.createServer(httpsOptions, app).listen(443);
--- router.js
+++ router.js
@@ -2,10 +2,6 @@ const { Router } = require('express');
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.sendFile(__dirname + "/homepage.html");
-});
-
 router.get("/info",(req, res) => {
     res.end(`You are (${req.ip}) visiting ${req.hostname} using ${req.protocol}.`);
 });
